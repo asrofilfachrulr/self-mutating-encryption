b3e5ca4d4e6c371d68888798e9869629ad189d5b
anya <riidloa@gmail.com>
2023-06-02 21:59:29 +0700

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+const wordList =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-={}[]|:;'\",<.>/?~`";
+
+const charPointerList = [];
+for (let i = 0; i < wordList.length; i++) charPointerList.push(i);
+
+console.log(charPointerList);
+
+function randomize(list) {
+  if (list.length < 1) return;
+}
+
+function encrypt(word, key) {}
+
+function decrypt(word, key) {}
